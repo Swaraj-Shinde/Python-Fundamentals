ea11fd88acfd6654488641e3469675c40e8a98f6
Swaraj Shinde <officially.swaraj@gmail.com>
2026-07-01 23:49:55 +0530

    Moved practise files to Corey-Scafers and added chapter 1.2 in cs61a

---
+++ cs61a/chapters/ch1.2.py
@@ -0,0 +1,22 @@
+#importing library functions
+from math import sqrt
+print(sqrt(256))
+
+from operator import add,sub,mul
+print(add(3,2))
+print(mul(4,5))
+print(sub(2,4))
+
+#assigning names
+radius = 10
+print(2*radius)
+
+#to give new names to old functions
+m=max
+print(m(2,3))
+
+#assigning values directly
+x,y=2,3
+print(x,y)
+y,x=x,y #swapping values
+print(x,y)
